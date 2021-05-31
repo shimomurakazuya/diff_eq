bd4eb430189cc5682e76c9e3ea86689d45137174
下村和也 <shimomurakazuya@SA-NAT23.tokai-sc.jaea.go.jp>
2021-05-31 09:28:40 +0900

    エルム規格化

--- src/Output.cpp
+++ src/Output.cpp
@@ -16,7 +16,7 @@
 void Output::
 setout(const ValuesDiffusion& v, const int t) {
 
-    float f_ave,f_max, err_elm;
+    float f_ave,f_max,f_ana, err_elm;
      f_ave = average(v);
      f_max = maximum(v);
      err_elm = analytic(v,t);
@@ -29,11 +29,13 @@ setout(const ValuesDiffusion& v, const int t) {
      ofs << "#t=" << std::setw(8) << t << "    x,    f,      average,	maximum, err_elm" << std ::endl;
 
     for(int i=0; i<defines::nx; i++) {
-     ofs << std::setw(8) << v.xx()[i] << " "
-         << std::setw(8) << v.ff()[i] << " "
-         << std::setw(8) << f_ave     << " "  
-         << std::setw(8) << f_max     << " "  
-         << std::setw(8) << err_elm     << " " << std::endl;
+      f_ana = defines::fmax * cos(v.xx()[i]/defines::lx*2.0*M_PI) * exp(- defines::c_dif* (2.0 * M_PI/defines::lx)* (2.0 * M_PI/defines::lx) * double(t)* defines::dt *defines::iout  );
+     ofs << std::setw(10) << v.xx()[i] << "  "
+         << std::setw(10) << v.ff()[i] << "  "
+         << std::setw(10) << f_ana     << "  "
+         << std::setw(10) << f_ave     << "  "  
+         << std::setw(10) << f_max     << "  "  
+         << std::setw(10) << err_elm     << " " << std::endl;
     }
 
     ofs.close(); 
@@ -78,8 +80,8 @@ analytic( const ValuesDiffusion& v,int t){
      real f_ana = 0; 
      real err_elm =0;
      for(int i=0; i<defines::nx; i++) { 
-      f_ana = defines::fmax * cos(v.xx()[i]/defines::lx*2.0*M_PI) * exp(-defines::c_dif * (M_PI/defines::lx)* (M_PI/defines::lx)*double(t) * defines::dt *defines::iout  );
-      err_elm = err_elm + fabs(f_ana - v.ff()[i])/defines::nx;
+      f_ana = defines::fmax * cos(v.xx()[i]/defines::lx*2.0*M_PI) * exp(-defines::c_dif * (2.0 * M_PI/defines::lx)* (2.0*M_PI/defines::lx)* t * defines::dt *defines::iout  );
+      err_elm = err_elm + fabs(f_ana - v.ff()[i]);
      }
      return err_elm;
 }
@@ -88,16 +90,20 @@ void Output::
 print_sum(const int t, const ValuesDiffusion& v) {
      double sum = 0, mmm = v.ff()[0];
      for(int i=0; i<defines::nx; i++) {
-         sum += v.ff()[i];
+         sum += fabs(v.ff()[i]);
          mmm = std::fmax(mmm, v.ff()[i]);
      }
-     float err_elm ;
+     float err_elm, elm, err_num ;
      err_elm = analytic(v,t);
+     elm = err_elm/sum;
+     err_num = err_elm/defines::nx;
      float Tout = t * defines::dt * defines::iout;
 
      std::cout << "t=" << std::setw(8) << Tout
-         << " :    " << std::setw(8) << sum
          << " :    " << std::setw(8) << mmm
-         << " ,    " << std::setw(8) << err_elm << std::endl;
+         << " :    " << std::setw(8) << sum
+         << " :    " << std::setw(8) << err_elm
+         << " :    " << std::setw(8) << err_num
+         << " ,    " << std::setw(8) << elm << std::endl;
 }
 
--- src/defines.h
+++ src/defines.h
@@ -1,14 +1,16 @@
 #ifndef DEFINE_H_
 #define DEFINE_H_
 
+#include<math.h>
+
 using real = float;
 namespace defines {
     // x, t
-    constexpr real lx = 12.5;
-    constexpr int nx = 100;
+    constexpr real lx = 1.0 ;
+    constexpr int nx = 800;
     constexpr int nc = nx / 2;
     constexpr real dx = lx / nx;
-    constexpr int iter = 21000;
+    constexpr int iter = 1000000;
     constexpr int iout = 100;
 //    constexpr int iter = 1;
 //    constexpr int iout = 1;
@@ -19,7 +21,6 @@ namespace defines {
     constexpr real delta = 4 * dx; // 4 dx
     constexpr real c_dif = 1;
 
-    // 4 err investigation
     constexpr real dt = 0.25* dx*dx/ c_dif ; //  4 investigation err
 
     // downsize
